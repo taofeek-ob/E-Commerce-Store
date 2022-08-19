f3127f021c667bafe33b6d2a39b55ea6059a6b6f
taofeek-ob <taofeek01@yahoo.com>
2022-08-20 00:08:17 +0100

    Refactored social media links

--- nextjs_ecommerce/components/Footer.js
+++ nextjs_ecommerce/components/Footer.js
@@ -1,16 +1,24 @@
 import Link from "next/link";
-import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiOutlineLinkedin,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <p>2022 Tawfiq All rights reserverd</p>
       <p className="icons" style={{ cursor: "pointer" }}>
-        <Link href="https://instagram.com/modestly.ng">
-          <AiFillInstagram />
+        <Link href="https://github.com/taofeek-ob">
+          <AiFillGithub />
         </Link>
-        <Link href="https://twitter.com/modestly_ng">
+        <Link href="https://twitter.com/taofeek_ob">
           <AiOutlineTwitter />
+        </Link>
+        <Link href="https://linkedin.com/in/taofeek-ob">
+          <AiOutlineLinkedin />
         </Link>
       </p>
     </div>
