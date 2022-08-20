283ec2645a92615355fe19859a7563cee1ac6bba
Taofeek Bakare <taofeek01@yahoo.com>
2022-08-20 06:50:03 +0100

    Update Navbar.js

--- nextjs_ecommerce/components/Navbar.js
+++ nextjs_ecommerce/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">TS Store</Link>
+        <Link href="/">TS Accessories Store</Link>
       </p>
 
       <button
